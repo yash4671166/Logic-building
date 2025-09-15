6d405724fdf0d7c25d486601a988fffd0eb82a58
yash4671166 <yashmahalpure23405@gmail.com>
2025-09-15 19:59:20 +0530

    updated one code

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+package Marvellous;
+
+public class Arithmetic
+{
+    public int Addition(int A , int B)
+    {
+        return A+B;
+    }
+    public int Subtraction(int A , int B)
+    {
+        return A-B;
+    }
+}
